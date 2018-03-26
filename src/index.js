88c5d26a8cad2628fa5a39cd4209df8460b38b2f
David Kironde <david.kironde@andela.com>
2018-03-26 21:13:27 +0300

    add handle click function and delete state from the square component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
       return (
-        <button className="square" onClick = {() => this.setState({value: 'X'})}>
-          {this.state.value}
+        <button className="square" onClick = {() => this.props.onClick({value: 'X'})}>
+          {this.props.value}
         </button>
       );
     }
@@ -20,14 +14,23 @@ class Square extends React.Component {
   
   class Board extends React.Component {
       constructor(props){
-          super(prop);
+          super(props);
           this.state = {
               squares: Array(9).fill(null),
           };
       }
+
+      handleClick(i) {
+          const squares = this.state.squares.slice();
+          squares[i] = 'X';
+          this.setState({squares: squares});
+      }
     renderSquare(i) {
-      return <Square value={i} />;
-    }
+      return( <Square value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)}
+    />
+);
+}
   
     render() {
       const status = 'Next player: X';
